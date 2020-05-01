07a6ddb02cd3345f8993e7b3c48e6b41ff652d3a
Michael Dotson <thedotson@icloud.com>
2020-04-30 19:56:59 -0500

    added print to dom function, click event function, and posting the form

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
-console.log('sorting hat')
+const newStudent = [];
+
+const house = ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin'];
+
+const printToDom = (selector, textToPrint) => {
+  const selectedDiv = document.querySelector(selector);
+  selectedDiv.innerHTML = textToPrint;
+}
+
+const buildForm = (event) => {
+  const buttonId = event.target.id;
+  let domString = '';
+
+  if (buttonId === 'createForm') {
+    domString += `
+    <form>
+      <div class="form-group">
+        <label for="exampleInputName">What is the name of the First Year?</label>
+        <input type="name" class="form-control form-control-sm" id="exampleInputName" aria-describedby="nameHelp" placeholder="Enter Name">
+        <small id="nameHelp" class="form-text text-muted">Please only include your legal First and Last name.</small>
+      </div>
+      <button id="sortStudent" type="submit" class="btn btn-secondary btn-sm">Submit</button>
+    </form>
+    `;
+  }
+  printToDom('#form', domString);
+};
+
+const clickEvents = () => {
+  document.querySelector('#createForm').addEventListener('click', buildForm)
+}
+
+const init = () => {
+  clickEvents();
+}
+
+init();
